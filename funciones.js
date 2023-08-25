180538953af869282b2515b6a57ae7c9b83330c5
proyectos hub <proyectos.hubservicescloud@gmail.com>
2023-08-25 17:06:27 -0500

    v2 curso

--- funciones.js
+++ funciones.js
@@ -6,3 +6,11 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1WRj6kxlzivYMbkN19ofKyLNxVYRrSM2PPWYluOVeGTA').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+
+    return datos;
+}
